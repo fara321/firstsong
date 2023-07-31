6a01e34526c39e66c089060ea4eb5a01ad132f63
fara321 <101867102+fara321@users.noreply.github.com>
2023-07-31 14:02:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+
+
+        function start12() {
+            window.open('text_regen.html');
+        }
+
+        function start11() {
+            window.open('text_laterne2.html');
+        }
+
+        function start0() {
+            window.open('text_laterne.html');
+        }
+
+        function start1() {
+            window.open('text_ente.html');
+        }
+
+        function start2() {
+            window.open('text_bienchen.html');
+        }
+
+        function start3() {
+            window.open('text_hase.html');
+        }
+
+        function start4() {
+            window.open('text_kuchen.html');
+        }
+
+        function start5() {
+            window.open('text_fuchs.html');
+        }
+
+        function start6() {
+            window.open('text_vögel.html');
+        }
+
+        function start7() {
+            window.open('text_hänsel.html');
+        }
+
+        function start8() {
+            window.open('text_kuckuck.html');
+        }
+
+          function start10() {
+            window.open('text_lalelu.html');
+        }
+
+        function start13() {
+            window.open('text_sleep.html');
+        }
+
+        function start14() {
+            window.open('text_butze.html');
+        }
+        function start15() {
+            window.open('geraeusche.html');
+        }
+
+
+    let alarm9 = new Audio('opa.mp3');
+
+    function start9() {
+                alarm9.play();
+            }   
+
+
+    let alarm16 = new Audio('meer.mp3')    
+    
+    function start16() {
+            alarm16.play();
+        }
+    
+        let alarm17 = new Audio('nachtigall.mp3')    
+    
+    function start17() {
+            alarm17.play();
+        }
+
+        let alarm18 = new Audio('cikade.mp3')    
+    
+    function start18() {
+            alarm18.play();
+        }
+
+
+    
